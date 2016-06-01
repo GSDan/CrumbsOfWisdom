3b81a2334ac5c7e9ece6cd0a47ea3fb8246599e1
NatalyBirbeck <nataly.birbeck@ncl.ac.uk>
2016-06-01 12:50:24 +0000

    Added button

--- PrinterBox/main.py
+++ PrinterBox/main.py
@@ -8,7 +8,6 @@ from time import sleep
 
  #GPIO pins
 gBut = 7       #button
-gLED = 10      #LED
 
 downloadsFolder = "./AdviceFiles"
 serverAddress = "http://46.101.42.140:1337/"
@@ -22,29 +21,58 @@ def RefreshAdvice():
 		    raise
 
     while True:
-        result = requests.get(serverAddress + "advice")
+        result = requests.get(serverAddress + "advice/getnew")
 
         for advice in result.json():
             localPath = os.path.join(downloadsFolder, os.path.basename(advice["filename"]))
 
             #Download the file if it doesn't exist locally
             if not os.path.isfile(localPath):
-                    params = {"fd" : advice["filename"]}
-                    r = requests.get(serverAddress + "file/download", params=params, stream=True)
+                params = {"fd" : advice["filename"]}
+                r = requests.get(serverAddress + "file/download", params=params, stream=True)
 
-                    print "DOWNLOADING:", advice["filename"]
+                print "DOWNLOADING:", advice["filename"]
 
-		    with open(localPath, 'wb') as f:
-			    for chunk in r.iter_content(chunk_size=1024): 
-				    if chunk: # filter out keep-alive new chunks
-					    f.write(chunk)
-					    f.flush()
+		with open(localPath, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=1024): 
+                        if chunk: # filter out keep-alive new chunks
+                            f.write(chunk)
+                            f.flush()
 				
-		    print "FINISHED:", localPath
+		print "FINISHED:", localPath
 
-		    subprocess.call(["python", "pipsta.py", localPath])
+	    else:
+
+                print "ALREADY CACHED", localPath
+
+		subprocess.call(["python", "pipsta.py", localPath])
            
         sleep(10)
 
 
-RefreshAdvice();
+#RefreshAdvice();
+
+try:
+        #initialising GPIO + pins + states
+        GPIO.setmode (GPIO.BOARD)
+        GPIO.setup (gBut, GPIO.IN, GPIO.PUD_UP)
+        butState = 1
+        
+        while True:
+                but = GPIO.input (gBut)
+                if but != butState:
+                        if but == 0:
+                                #if button is pressed
+                                print "off"
+
+                        else:
+                                        
+                                print "printing image"
+                                sleep(1)
+                                
+                butState = but
+                sleep (0.1)
+
+finally:
+        GPIO.cleanup
+    
