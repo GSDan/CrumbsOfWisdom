7c006592b8609e6e0722ee0d0d457b40422f2792
Dan Richardson <dan.richardson.gs@gmail.com>
2016-06-07 14:02:30 +0100

    Thread locking and better error handling

--- biscuitBox/main.py
+++ biscuitBox/main.py
@@ -15,7 +15,8 @@ gCamBut = 15 # Take photo button
 gSkipBut = 22 # Skip question button
 gLED = 7 # LED
 
-downloadsFolder = "./QuestionFiles"
+thisDir = os.path.dirname(os.path.abspath(__file__))
+downloadsFolder = thisDir + "/QuestionFiles"
 serverAddress = "http://46.101.42.140:1337/"
 pollIntervalMinutes = 1
 
@@ -25,7 +26,9 @@ lastQuestionId = "";
 lastPlayed = "";
 canTakePhoto = False;
 hasTakenPhoto = False;
-tempImageFile = "image.jpg"
+tempImageFile = thisDir + "/image.jpg"
+
+lock = threading.Lock()
 
 def TakeAndCropPhoto():
 	camera = picamera.PiCamera()
@@ -36,7 +39,7 @@ def TakeAndCropPhoto():
 	camera.vflip = True
 	camera.capture(tempImageFile)
 	
-	subprocess.call(['mplayer', "camera-snap.wav"])
+	subprocess.call(['mplayer', thisDir + "/camera-snap.wav"])
 
 	# Crop image& up the contrast
 	original = Image.open(tempImageFile)
@@ -52,17 +55,21 @@ def TakeAndCropPhoto():
 
 def UploadImage():
 	print "Uploading " + tempImageFile
-	with open(tempImageFile, 'rb') as payload:
-		files = {"image" : payload}
-		data = {"questionId" : lastQuestionId }
-		res = requests.post(serverAddress + "advice/upload", files = files, data = data)
-		
-		if res.status_code == 200:
-			print "UPLOAD RETURN: " + str(res.json())
-			return True
-		else:
-			print "Upload failed!!"
-			return False
+	try:
+		with open(tempImageFile, 'rb') as payload:
+			files = {"image" : payload}
+			data = {"questionId" : lastQuestionId }
+			res = requests.post(serverAddress + "advice/upload", files = files, data = data)
+			
+			if res.status_code == 200:
+				print "UPLOAD RETURN: " + str(res.json())
+				return True
+			else:
+				print "Upload failed!!"
+				return False
+	except requests.exceptions.RequestException as e:    # This is the correct syntax
+		print e
+		return False
 
 # Checks for new questions on the server
 # If a question doesn't exist locally, download it
@@ -70,28 +77,32 @@ def UploadImage():
 def RefreshQuestions():
 	while True:
 		print "POLLING SERVER"
-		res = requests.get(serverAddress + "question/getnew")
 
-		for question in res.json():
-			localPath = os.path.join(downloadsFolder, os.path.basename(question["id"] + ".mp3"))
-			
-			# Download the file if it doesn't exist locally
-			if not os.path.isfile(localPath):
-				params = {"fd" : question["filename"] }
-				r = requests.get(serverAddress + "file/download", params=params, stream=True)
+		try:
+			res = requests.get(serverAddress + "question/getnew")
 
-				print "DOWNLOADING:", question["filename"]
-
-				with open(localPath, 'wb') as f:
-					for chunk in r.iter_content(chunk_size=1024): 
-						if chunk: # filter out keep-alive new chunks
-							f.write(chunk)
-							f.flush()
+			for question in res.json():
+				localPath = os.path.join(downloadsFolder, os.path.basename(question["id"] + ".mp3"))
 				
-				print "FINISHED:", localPath 		
-			
-			else:
-				print "ALREADY CACHED:", localPath
+				# Download the file if it doesn't exist locally
+				if not os.path.isfile(localPath):
+					params = {"fd" : question["filename"] }
+					r = requests.get(serverAddress + "file/download", params=params, stream=True)
+
+					print "DOWNLOADING:", question["filename"]
+
+					with open(localPath, 'wb') as f:
+						for chunk in r.iter_content(chunk_size=1024): 
+							if chunk: # filter out keep-alive new chunks
+								f.write(chunk)
+								f.flush()
+					
+					print "FINISHED:", localPath 		
+				
+				else:
+					print "ALREADY CACHED:", localPath
+		except requests.exceptions.RequestException as e:    # This is the correct syntax
+			print e
 
 		sleep(60 * pollIntervalMinutes)
 
@@ -101,34 +112,39 @@ def RefreshQuestions():
 def CheckLightLevels():
 	global currentLightLevel
 
-	window = [0,0,0,0,0] # 1 second window
-	lastReading = 0
-	
-	GPIO.setup(gLED, GPIO.OUT)
+	try:
+		window = [0,0,0,0,0] # 1 second window
+		lastReading = 0
+		
+		GPIO.setup(gLED, GPIO.OUT)
 
-	while True:
+		while True:
 
-		# Ground the pin to empty the capacitor
-		GPIO.setup(gLight, GPIO.OUT)
-		GPIO.output(gLight, GPIO.LOW)
-		sleep(0.2)
-	
-		# Set as an input
-		GPIO.setup(gLight, GPIO.IN)	
+			# Ground the pin to empty the capacitor
+			GPIO.setup(gLight, GPIO.OUT)
+			GPIO.output(gLight, GPIO.LOW)
+			sleep(0.2)
+		
+			# Set as an input
+			GPIO.setup(gLight, GPIO.IN)	
 
-		startTime = time()	
-		while GPIO.input(gLight) == GPIO.LOW: pass
-		elapsed = int((time() - startTime) * 1000000)	
-	
-		# Add data to rolling window
-		window.append(elapsed)
-		del window[0]
+			startTime = time()	
+			while GPIO.input(gLight) == GPIO.LOW: pass
+			elapsed = int((time() - startTime) * 1000000)	
+		
+			# Add data to rolling window
+			window.append(elapsed)
+			del window[0]
 
-		# Rest of the program is given the window's average
-		currentLightLevel = (sum(window) / float(len(window)))
+			with lock:
+				# Rest of the program is given the window's average
+				currentLightLevel = (sum(window) / float(len(window)))
 
-		# Update the LED - light if there are files available
-		GPIO.output(gLED, len(os.listdir(downloadsFolder)));
+			# Update the LED - light if there are files available
+			GPIO.output(gLED, len(os.listdir(downloadsFolder)));
+	finally:
+		GPIO.cleanup()
+		os._exit()
 		
 # Checks whether or not the buttons have been pressed
 # If gCamBut has been and if permitted, take a photo and upload it
@@ -137,48 +153,52 @@ def CheckButtonStatus():
 	global canTakePhoto
 	global hasTakenPhoto
 
-	GPIO.setup (gCamBut, GPIO.IN, GPIO.PUD_UP)
-	GPIO.setup (gSkipBut, GPIO.IN, GPIO.PUD_UP)
-	camButPrevState = 1
-	skipButPrevState = 1
+	try:
+		GPIO.setup (gCamBut, GPIO.IN, GPIO.PUD_UP)
+		GPIO.setup (gSkipBut, GPIO.IN, GPIO.PUD_UP)
+		camButPrevState = 1
+		skipButPrevState = 1
 
-	while True:
-		camButCurrState = GPIO.input (gCamBut)
-		if camButCurrState != camButPrevState:
-			if camButCurrState == 1:
+		while True:
+			camButCurrState = GPIO.input (gCamBut)
+			if camButCurrState != camButPrevState:
+				if camButCurrState == 1:
 
-				if canTakePhoto and not hasTakenPhoto:
-					print "Taking photo"
-					TakeAndCropPhoto()
-					
-					success = UploadImage()
+					if canTakePhoto and not hasTakenPhoto:
+						print "Taking photo"
+						TakeAndCropPhoto()
+						
+						success = UploadImage()
 
-					if success:
-						print "Deleting file:", lastPlayed
-						os.remove(lastPlayed)
+						if success:
+							print "Deleting file:", lastPlayed
+							os.remove(lastPlayed)
 
-				else:
-					print "Can't take a photo right now"
+					else:
+						print "Can't take a photo right now"
 
-		skipButCurrState = GPIO.input (gSkipBut)
+			skipButCurrState = GPIO.input (gSkipBut)
 
-		if skipButCurrState != skipButPrevState:
-			if skipButCurrState == 1:
-				print "Skip button!"
+			if skipButCurrState != skipButPrevState:
+				if skipButCurrState == 1:
+					print "Skip button!"
 
-				data = {"questionId" : lastQuestionId }
-				res = requests.post(serverAddress + "question/dismiss", data = data)
-				os.remove(lastPlayed)
+					data = {"questionId" : lastQuestionId }
+					res = requests.post(serverAddress + "question/dismiss", data = data)
+					os.remove(lastPlayed)
 
-				if os.listdir(downloadsFolder):
-					PlayQuestion()
-				else:
-					print "No more messages"
+					if os.listdir(downloadsFolder):
+						PlayQuestion()
+					else:
+						print "No more messages"
 
-		camButPrevState = camButCurrState
-		skipButPrevState = skipButCurrState
+			camButPrevState = camButCurrState
+			skipButPrevState = skipButCurrState
 
-		sleep (0.1)
+			sleep (0.1)
+	finally:
+		GPIO.cleanup()
+		os._exit()
 
 # Play a random downloaded question audio file
 def PlayQuestion():
@@ -227,7 +247,8 @@ try:
 	while True:
 		sleep(1.5)
 
-		tinOpen = (currentLightLevel > closedLightLevel)
+		with lock:
+			tinOpen = (currentLightLevel > closedLightLevel)
 
 		if not tinOpen:
 			if hasPlayed:
